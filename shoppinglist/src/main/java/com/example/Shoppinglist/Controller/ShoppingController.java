78c2552824dabae51560e49d589a4f76af51d60c
Thinh Nguyen <thinhschlange2001@gmail.com>
2023-12-04 20:34:49 +0000

    OpenTelemetry

--- shoppinglist/src/main/java/com/example/Shoppinglist/Controller/ShoppingController.java
+++ shoppinglist/src/main/java/com/example/Shoppinglist/Controller/ShoppingController.java
@@ -17,6 +17,7 @@ import com.example.Shoppinglist.Model.Shopping;
 import com.example.Shoppinglist.Service.ShoppingService;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import io.opentelemetry.instrumentation.annotations.WithSpan;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -26,11 +27,14 @@ public class ShoppingController {
     @Autowired
     private ShoppingService shoppingService;
 
+
+    @WithSpan
     @GetMapping("/shopping")
     ResponseEntity<List<Shopping>> getAllShoppingItems(){
         return new ResponseEntity<>(shoppingService.getAllShoppingItems(),HttpStatus.OK);
     }
 
+    @WithSpan
     @PostMapping("/shopping")
     ResponseEntity<Shopping> createShoppingItem(@RequestBody Shopping shopping){
         Shopping createdShoppingItem = shoppingService.createShoppingItem(shopping);
@@ -38,17 +42,21 @@ public class ShoppingController {
         return ResponseEntity.ok(createdShoppingItem);
     }
 
+    @WithSpan
     @DeleteMapping("/shopping/{id}")
     ResponseEntity<String> deleteShoppingItem(@PathVariable Integer id){
         shoppingService.deleteShoppingItem(id);
         return ResponseEntity.ok("Deleted");
     }
+
+    @WithSpan
     @DeleteMapping("/shopping")
     ResponseEntity<String> deleteAllShoppingItem(){
         shoppingService.deleteAllShoppingItem();
         return ResponseEntity.ok("Deleted All Shopping Items");
     }
 
+    @WithSpan
     @PutMapping("/shopping/{id}")
     ResponseEntity<Shopping> updateShoppingItem(@PathVariable Integer id, @RequestBody Shopping shopping){
         return ResponseEntity.ok(shoppingService.updateShopping(id,shopping));
